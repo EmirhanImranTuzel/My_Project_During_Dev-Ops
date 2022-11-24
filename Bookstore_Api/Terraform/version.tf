2d2ec67a1e206f7eb7bd6e6bbd59b0fefec2e51e
EmirhanImranTuzel <emrhntzl@gmail.com>
2022-11-24 07:10:25 +0100

    Solved

--- Bookstore_Api/Terraform/version.tf
+++ Bookstore_Api/Terraform/version.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    # github = {
-    #   source = "integrations/github"
-    #   version = "5.9.1"
-    # }
+    github = {
+      source = "integrations/github"
+      version = "5.9.1"
+    }
   }
 }
 
